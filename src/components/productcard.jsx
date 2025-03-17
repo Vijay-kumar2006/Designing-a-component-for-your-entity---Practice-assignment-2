1142186799a752399c36ce3cff2a59f3d0e15b11
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-03-17 21:33:00 +0530

    Update productcard.jsx

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,21 +1,24 @@
-// write product card here
-import react from 'react'
+import React from 'react';
+import ViewProduct from './button';
 
-import viweProduct from '../components/button.jsx';
 
-const productDetails=()=>{
+const Productcard = () => {
   const productImage = 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRR92jpaoZTwtMew82LmQQ2fr2v3MPsidd5aqEnIhYfz7dJX2KEwwZfeIn-SvNXIGiQlPhHD7_Alk2-sb7iLxFuR9vb1U3SL_NJ15VhlPv_2-bpqSt-Dj1b&usqp=CAE';
-  const productName = 'Samsung galaxy s24 ultra';
-  const price = "1,59,000.00"
+  const productName = 'Samsung Galaxy S24 Ultra';
+  const price = '₹1,59,000';
 
   return (
-    <div className="product-card">
-      <img src={productImage} alt="{}"/>
-      <h2>Name:  {productName}</h2>
-      <h2>Price : {price}</h2>
-      <viweProduct/>
+    <div >
+      <img src={productImage} alt={productName}  />
+      <h2>{productName}</h2>
+      <h3>Price: {price}</h3>
+      <ViewProduct />
     </div>
   );
 };
 
-export default productDetails;
+
+
+ 
+
+export default Productcard;
